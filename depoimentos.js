b09aba70c3095956f32f55932aa2f5c111786618
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-02-04 15:45:39 -0300

    project finished

--- depoimentos.js
+++ depoimentos.js
@@ -1,7 +1,7 @@
 const reviews = [
 	{
 		id: 1,
-		name: "Augusto Souza",
+		name: "Márcia Gouveia",
 		text: "Quero aqui fazer um agradecimento a todos os envolvidos neste grande empreendimento chamado \"Intercâmbio Inglaterra\", que, para os nossos filhos, abrirão muitas portas de trabalho. Também e principalmente agradecer a Deus que nos permitiu fazer esta viagem maravilhosa e agradecer a nossa querida Consuelo que tanto se empenha para que isso aconteça da melhor forma possível e que trabalha muito. Obrigada Deus, obrigada Consuelo, obrigada, obrigada e obrigada!!!!",
 	},
 	{
@@ -11,7 +11,7 @@ const reviews = [
 	},
 	{
 		id: 3,
-		name: "Marouleize",
+		name: "Consuelo Santos",
 		text: "Realizamos com louvor o 25º CULTURA TRIP INGLATERRA 2020! Com a Graça de Deus, sempre nos guiando, cumprimos com sucesso e relevância todas as etapas de mais um Intercâmbio!! Cada viagem é única, cada grupo é único! Poderíamos reunir em um livro todas as nossas experiências, que a cada ano também são únicas! Nós, os personagens, também somos únicos, pois cada um de nós tem sua própria essência!! Vivemos com intensidade todos os nossos momentos! Embora o tempo possa ter passado relativamente rápido, a sensação é de que já estamos tanto tempo juntos!! E ao mesmo tempo afastados da Família do Brasil! Sim, porque formamos aqui nossa própria Família. Em tão curto período de tempo, como expandimos as nossas mentes!! Como entendemos melhor hoje as diferenças e o quão rápido nos adaptamos a elas!! Novos horizontes se abrirão para nós a partir desse Intercâmbio!!",
 	}
 ];
